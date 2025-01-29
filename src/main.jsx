76b49598ee78f5ca3165f50b4895e4d883938ec9
Boban <bozicboban002@gmail.com>
2025-01-29 13:11:07 +0100

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { App } from './components/App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
